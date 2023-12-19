bb18d929da1e53f4d5eee7ec5c907eca4529311f
keet <keet@huelsdau.com>
2023-12-19 11:46:54 +0100

    Initial commit

---
+++ src/main/java/com/example/aw_finalprojectbackend/AwFinalProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aw_finalprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwFinalProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwFinalProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aw_finalprojectbackend/AwFinalProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aw_finalprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwFinalProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
