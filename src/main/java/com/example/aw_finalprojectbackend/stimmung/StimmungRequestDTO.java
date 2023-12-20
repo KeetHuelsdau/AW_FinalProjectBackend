56c52aec9f86c406a9b4cc85cd31bc3a5cb23ad0
Moritz Rose <moritz_rose@yahoo.de>
2023-12-20 15:43:38 +0100

    erster FullMerge mit Stimmung und KRypto

--- src/main/java/com/example/aw_finalprojectbackend/stimmung/StimmungRequestDTO.java
+++ src/main/java/com/example/aw_finalprojectbackend/stimmung/StimmungRequestDTO.java
@@ -1,31 +1,3 @@
 package com.example.aw_finalprojectbackend.stimmung;
 
-public class StimmungRequestDTO {
-    Long stimmungId;
-    int rating;
-    String kommentar;
-
-    public Long getStimmungId() {
-        return stimmungId;
-    }
-
-    public void setStimmungId(Long stimmungId) {
-        this.stimmungId = stimmungId;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getKommentar() {
-        return kommentar;
-    }
-
-    public void setKommentar(String kommentar) {
-        this.kommentar = kommentar;
-    }
-}
+public record StimmungRequestDTO(int rating, String kommentar) {}
